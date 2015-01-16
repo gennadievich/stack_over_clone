f5593effc773a70a85877a28433486ecaa21e553
Kirill <ay@mail.by>
2015-01-16 20:01:46 +0300

    added User Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
 
-  def index
+  def show
+    begin
+      @user = User.find(params[:id])
+      unless @user == current_user
+        redirect_to root_path
+      end
+    rescue
+      redirect_to root_path
+    end
 
   end
 

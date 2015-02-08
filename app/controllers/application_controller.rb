cddf773902c1e7282dd599edf46cc8e5d9defae8
Kirill <ay@mail.by>
2015-02-08 12:24:08 +0300

    more csssss!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :current_user, :login, :logout, :authenticate_user
+  helper_method :current_user, :login, :logout, :authenticate_user, :check_current_user
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -20,6 +20,10 @@ class ApplicationController < ActionController::Base
     if current_user.nil?
       redirect_to login_path
     end
+  end
+
+  def check_current_user(user)
+    user == current_user
   end
 
 end
